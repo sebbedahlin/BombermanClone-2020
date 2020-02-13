f65be467fd092e8ecc481cd08b4d151c9ef407f8
sebbedahlin <sebbedahlin@gmail.com>
2020-02-13 14:54:06 +0100

    yea

---
+++ Assets/TileGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject tile;
+
+    [SerializeField]
+    int width = 8;
+
+    [SerializeField]
+    int height = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int width = 8;
+        int height = 8;
+
+        Vector3 offset = new Vector3(
+            -width/2f + 0.5f,
+            -height/2f + 0.5f
+            );
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                MakeTile(offset, x, y);
+            }
+        }
+    }
+
+    protected virtual void MakeTile(Vector3 offset, int x, int y)
+    {
+        GameObject newTile = Instantiate(tile,
+            new Vector3(x, y, transform.position.z) + offset,
+            Quaternion.identity);
+
+        newTile.name = tile.name + " " + x + "," + y;
+        newTile.transform.parent = transform;
+    }
+}
---
+++ Assets/WallGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallGenerator : TileGenerator
+{
+
+    protected override void MakeTile(Vector3 offset, int x, int y)
+    {
+        if (x == 0)
+        {
+            base.MakeTile(offset, x, y);
+        }
+        else if (x == 2 && y == 1)
+        {
+            base.MakeTile(offset, x, y);
+        }
+    }
+}
